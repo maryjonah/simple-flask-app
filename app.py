e736d60969f36bff05eed9116052766b3325a809
Mary Jonah <jonahmary17@gmail.com>
2024-06-07 12:09:39 +0000

    return json output instead

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello World!</p>"
+  return jsonify({'hello': 'from template api auto-deployed with GitHub actions and being tested!'}), 200
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
-  
+  app.run(host='0.0.0.0', debug=True)  
